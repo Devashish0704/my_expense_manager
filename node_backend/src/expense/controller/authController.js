1f8c70eda5eb4b397b23f54367447ccdabbca70f
Devashish0704 <dev070704@gmail.com>
2024-07-31 14:28:21 +0530

    User name2

--- node_backend/src/expense/controller/authController.js
+++ node_backend/src/expense/controller/authController.js
@@ -38,7 +38,7 @@ const googleUser = async (req, res) => {
     // Generate JWT
     const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
 
-    res.json({ id: user.id, token, name: newUser.name });
+    res.json({ id: user.id, token, name: user.name });
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: 'Token verification failed' });
@@ -83,7 +83,7 @@ const loginUser = async (req, res) => {
       if (isMatch) {
         const payload = { id: user.id, username: user.username };
         const token = jwt.sign(payload, secretOrKey, { expiresIn: '1h' });
-        res.status(200).json({ token, id: user.id, name: newUser.name });
+        res.status(200).json({ token, id: user.id, name: user.name });
       } else {
         res.status(400).json({ error: 'Password incorrect' });
       }
