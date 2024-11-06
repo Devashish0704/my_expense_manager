03fa4faa1606b3e03a17305fca3311f5afe16bc9
Devashish0704 <dev070704@gmail.com>
2024-11-06 20:32:57 +0530

    Corrected server.js 3

--- node_backend/server.js
+++ node_backend/server.js
@@ -8,10 +8,10 @@ const pool = require("./db");
 const app = express();
 
 // Middleware
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Define routes
